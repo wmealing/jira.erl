6e903824258cec141ba905e434994dd09bc606a6
Wade Mealing <wmealing@redhat.com>
2025-08-29 21:29:08 +1000

    First attempt at adding update functionality, also cleaned up auth since i'm re-using it.

--- src/jira.erl
+++ src/jira.erl
@@ -1,14 +1,18 @@
 -module( jira ).
 -author( "Warren Kenny <warren.kenny@gmail.com>" ).
 
--export( [init/5, init/4, init/3, url/1, issue/2, search/3, search/5, jql/1] ).
+-export( [init/5, init/4, init/3, url/1, issue/2, search/3, search/5, jql/1, update_issue/3, get_myself/1] ).
+
+-export( [key_from_issue/1, field_from_issue/2]).
 
 -ifdef( TEST ).
 -export( [json_get/3] ).
 -endif.
 
--record( state, {   username        :: binary(),
-                    password        :: binary(),
+-record( state, {   username        :: binary() | undefined,
+                    password        :: binary() | undefined,
+                    authtype        :: atom()   | undefined,
+                    api_key         :: binary() | undefined,
                     url             :: binary(),
                     jsx_options     :: proplists:proplist()
 } ).
@@ -27,14 +31,22 @@ init( Username, Password, Host, Port, JSXOptions ) ->
                 jsx_options = JSXOptions
     }.
     
+-spec init( string(), string(), proplists:proplist() ) -> #state{}.
+init(bearerauth, Host, JSXOptions) ->
+    #state{
+       authtype   = bearerauth,
+       url         = want:binary( "https://" ++ Host ++ ":" ++ "443" ++ "/rest/api/2" ),
+       jsx_options = JSXOptions
+    };
+
+init( Username, Password, Host ) ->
+    init( Username, Password, Host, 443, [] ).
+
 -spec init( string(), string(), string(), proplists:proplist() ) -> #state{}.
 init( Username, Password, Host, JSXOptions ) ->
     init( Username, Password, Host, 443, JSXOptions ).
-    
--spec init( string(), string(), string() ) -> #state{}.
-init( Username, Password, Host ) ->
-    init( Username, Password, Host, 443, [] ).
-    
+
+
 %%
 %%  Retrieve the REST API URL for the given state
 %%
@@ -45,14 +57,52 @@ url( #state{ url = URL } ) -> URL.
 %%  Get the issue with the given key
 %%
 -spec issue( string(), #state{} ) -> { ok, issue() } | { error, term() }.
-issue( Key, #state{ url = BaseURL, username = Username, password = Password, jsx_options = JSXOptions } ) ->
+issue( Key, State = #state{ url = BaseURL, jsx_options = JSXOptions } ) ->
     URL = want:binary( url:join( BaseURL, [ "issue", Key ] ) ),
-    Options = [{ basic_auth, { Username, Password } } ],
-    case hackney:get( URL, [], <<>>, Options ) of
+    {Headers, Options} = get_auth_headers_and_options(State),
+    case hackney:get( URL, Headers, <<>>, Options ) of
         { ok, 200, _Headers, Ref } ->
             { ok, ResponseBody } = hackney:body( Ref ),
             Issue = jsx:decode( ResponseBody, lists:append( [ return_maps ], JSXOptions ) ),
             { ok, Issue };
+        { ok, _Status, _Headers, Ref } ->
+            { ok, ErrorBody } = hackney:body( Ref ),
+            { error, ErrorBody };
+        { error, Reason } ->
+            { error, want:binary( Reason ) }
+    end.
+
+%%
+%%  Update an issue with the given key using the provided fields map
+%%
+-spec update_issue( string(), map(), #state{} ) -> ok | { error, term() }.
+update_issue( Key, Fields, State = #state{ url = BaseURL } ) ->
+    URL = want:binary( url:join( BaseURL, [ "issue", Key ] ) ),
+    Body = #{ fields => Fields },
+    {Headers, Options} = get_auth_headers_and_options(State),
+    AllHeaders = [ {<<"Content-Type">>, <<"application/json">> } | Headers ],
+    case hackney:put( URL, AllHeaders, jsx:encode( Body ), Options ) of
+        { ok, 204, _Headers, _Ref } ->
+            ok;
+        { ok, _Status, _Headers, Ref } ->
+            { ok, ErrorBody } = hackney:body( Ref ),
+            { error, ErrorBody };
+        { error, Reason } ->
+            { error, want:binary( Reason ) }
+    end.
+
+%%
+%%  Get current user information to test authentication
+%%
+-spec get_myself( #state{} ) -> { ok, map() } | { error, term() }.
+get_myself( State = #state{ url = BaseURL, jsx_options = JSXOptions } ) ->
+    URL = want:binary( url:join( BaseURL, [ "myself" ] ) ),
+    {Headers, Options} = get_auth_headers_and_options(State),
+    case hackney:get( URL, Headers, <<>>, Options ) of
+        { ok, 200, _Headers, Ref } ->
+            { ok, ResponseBody } = hackney:body( Ref ),
+            UserInfo = jsx:decode( ResponseBody, lists:append( [ return_maps ], JSXOptions ) ),
+            { ok, UserInfo };
         { ok, _Status, _Headers, Ref } ->
             { ok, ErrorBody } = hackney:body( Ref ),
             { error, ErrorBody };
@@ -90,15 +140,16 @@ json_get( Key, JSON, #state{ jsx_options = JSXOptions } ) ->
 %%  of results. On success, returns the total number of results available as well as the list of retrieved issues.
 %%
 -spec search( string(), integer(), integer(), [binary()], #state{} ) -> { ok, [issue()] } | { error, term() }.
-search( JQL, Start, Max, Fields, State = #state{ username = Username, password = Password, url = BaseURL, jsx_options = JSXOptions } ) ->
+search( JQL, Start, Max, Fields, State = #state{ url = BaseURL, jsx_options = JSXOptions } ) ->
     Body = #{   jql         => want:binary( JQL ),
                 startAt     => Start,
                 maxResults  => Max,
                 fields      => [ want:binary( F ) || F <- Fields ]                
     },
-    Options = [{ basic_auth, { Username, Password } } ],
     URL = want:binary( url:join( BaseURL, [ "search" ] ) ),
-    case hackney:post( URL, [{ <<"Content-Type">>, <<"application/json">> }], jsx:encode( Body ), Options ) of
+    {Headers, Options} = get_auth_headers_and_options(State),
+    AllHeaders = [ {<<"Content-Type">>, <<"application/json">> } | Headers ],
+    case hackney:post( URL, AllHeaders, jsx:encode( Body ), Options ) of
         { ok, 200, _Headers, Ref } ->
             { ok, ResponseBody } = hackney:body( Ref ),
             ResponseJSON = jsx:decode( ResponseBody, lists:append( [ return_maps ], JSXOptions ) ),
@@ -136,3 +187,29 @@ jql_operator( _ )           -> "".
 jql_value( { { Y, M, D }, { HH, MM, _SS } } )       -> lists:flatten( io_lib:format( "'~w/~2..0w/~2..0w ~2..0w:~2..0w'", [Y, M, D, HH, MM] ) );
 jql_value( { { Y, M, D }, { HH, MM, _SS, _MS } } )  -> lists:flatten( io_lib:format( "'~w/~2..0w/~2..0w ~2..0w:~2..0w'", [Y, M, D, HH, MM] ) );
 jql_value( Value )                                  -> lists:append( ["'", want:string( Value ), "'" ] ).
+
+
+get_auth_headers_and_options(#state{ authtype = bearerauth }) ->
+    ApiKey = os:getenv("JIRA_KEY"),
+    AuthKey = <<"Authorization">>,
+    BearerPrefix = <<"Bearer ">>,
+    ApiKeyBinary = list_to_binary(ApiKey),
+    BearerValue = <<BearerPrefix/binary, ApiKeyBinary/binary>>,
+    {[{AuthKey, BearerValue}], []};
+
+get_auth_headers_and_options(#state{ username = Username, password = Password }) ->
+    {[], [{ basic_auth, { Username, Password } }]}.
+
+
+%% Helper functions.
+from_issue(Issue, Key) ->
+    #{Key := Value} = Issue,
+    Value.
+
+key_from_issue(Issue) ->
+    from_issue(Issue, <<"key">>).
+
+field_from_issue(Issue, Key) ->
+    Fields = from_issue(Issue, <<"fields">>),
+    #{Key := Value} = Fields,
+    Value.
--- test/jira_tests.erl
+++ test/jira_tests.erl
@@ -4,13 +4,74 @@
 
 -include_lib( "eunit/include/eunit.hrl" ).
 
-init_test() ->
-	State = jira:init( "user", "password", "jira.example.com" ),
-	?assert( jira:url( State ) =:= <<"https://jira.example.com:443/rest/api/2">> ).
+%% Test suite setup and teardown
+jira_test_() ->
+    {setup,
+     fun setup/0,
+     fun teardown/1,
+     [
+      fun test_init/0,
+      fun test_jql/0,
+      fun test_json_get/0,
+      fun test_update_issue/0,
+      fun test_get_myself/0
+     ]}.
 
-jql_test() ->
-	"project = 'VULN' AND updated > '2016/07/06 20:34'" = jira:jql( [{ project, eq, "VULN" }, 'and', { updated, gt, { { 2016, 07, 06 }, { 20, 34, 00 } } }] ).
+setup() ->
+    hackney:start().
 
-json_get_test() ->
-	<<"Hello">> = jira:json_get( key, #{ <<"key">> => <<"Hello">> }, jira:init( "user", "password", "jira.example.com", 80, [] ) ),
-	<<"Hello">> = jira:json_get( key, #{ key => <<"Hello">> }, jira:init( "user", "password", "jira.example.com", 80, [{ labels, atom }] ) ).
+teardown(_) ->
+    hackney:stop().
+
+test_init() ->
+	State = jira:init( bearerauth, "issues.redhat.com", [] ),
+	?assert( jira:url( State ) =:= <<"https://issues.redhat.com:443/rest/api/2">> ).
+
+test_jql() ->
+    "project = 'VULN' AND updated > '2016/07/06 20:34'" = jira:jql( [{ project, eq, "VULN" }, 'and', { updated, gt, { { 2016, 07, 06 }, { 20, 34, 00 } } }] ).
+
+test_json_get() ->
+	<<"Hello">> = jira:json_get( key,
+                                     #{ <<"key">> => <<"Hello">> }, 
+                                     jira:init( "user", "password", "jira.example.com", 80, [] ) ),
+
+    <<"Hello">> = jira:json_get( key, 
+                                 #{ key => <<"Hello">> }, 
+                                 jira:init( "user", "password", "jira.example.com", 
+                                            80, [{ labels, atom }] ) ).
+
+test_update_issue() ->
+    Key = "KMAINT-1000",
+    Fields = #{ <<"customfield_12310243">> => 11 },
+    State = jira:init(bearerauth, "issues.redhat.com", []),
+    
+    %% First get the current issue to verify it exists
+    case jira:issue(Key, State) of
+        {ok, OriginalIssue} ->
+            %% Update the issue
+            UpdateResult = jira:update_issue(Key, Fields, State),
+            ?assertMatch(ok, UpdateResult),
+            
+            %% Fetch the issue again to verify the update
+            {ok, UpdatedIssue} = jira:issue(Key, State),
+            UpdatedFields = jira:field_from_issue(UpdatedIssue, <<"customfield_12310243">>),
+            %% JIRA may return the value as float, so check for both
+            ?assert(UpdatedFields =:= 11 orelse UpdatedFields =:= 11.0);
+        {error, _Reason} ->
+            %% If we can't fetch the issue (likely auth/network), skip the test
+            ?assert(true)
+    end.
+
+test_get_myself() ->
+    State = jira:init(bearerauth, "issues.redhat.com", []),
+    
+    %% Test authentication by getting current user info
+    case jira:get_myself(State) of
+        {ok, UserInfo} ->
+            %% Verify we got a user info map with expected fields
+            ?assert(is_map(UserInfo)),
+            ?assert(maps:is_key(<<"name">>, UserInfo) orelse maps:is_key(<<"accountId">>, UserInfo));
+        {error, _Reason} ->
+            %% If auth fails or network issues, skip the test
+            ?assert(true)
+    end.
